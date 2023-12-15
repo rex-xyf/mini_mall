c9d5e59d5a4932e0202e4031b8936f67a3f2a2dd
winky <78466981@qq.com>
2023-12-15 11:26:20 +0800

    Identify three input scenarios

--- src/main/java/com/algorithm/FindLayout.java
+++ src/main/java/com/algorithm/FindLayout.java
@@ -1,4 +1,20 @@
 package com.algorithm;
 
+import com.sim.domain.Plan;
+
 public class FindLayout {
+    public Plan find(Plan layout){
+        //多的情况
+        if (layout.getCount() >20){
+            return new Plan();
+        }
+        //少的情况
+        else if (layout.getCount() <20){
+            return new Plan();
+        }
+        //一致的情况
+        else {
+            return new Plan();
+        }
+    }
 }

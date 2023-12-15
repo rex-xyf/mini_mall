a6b2585f3dd2589cfbb9c4871ae600d7bad1cdd0
winky <78466981@qq.com>
2023-12-15 20:46:31 +0800

    Change the domain class of Plan and front-end

--- src/main/java/com/algorithm/FindLayout.java
+++ src/main/java/com/algorithm/FindLayout.java
@@ -6,10 +6,13 @@ public class FindLayout {
     public Plan find(Plan layout){
         //多的情况
         if (layout.getCount() >20){
+            //需要考虑类型比例，衣服6，商店6，餐饮5，娱乐3
+
             return new Plan();
         }
         //少的情况
         else if (layout.getCount() <20){
+            //首先锁定大型店铺，之后是中型店铺，之后是小型店铺，按人均消费
             return new Plan();
         }
         //一致的情况
@@ -17,4 +20,6 @@ public class FindLayout {
             return new Plan();
         }
     }
+
+
 }

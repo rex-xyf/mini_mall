89132c2113fc0688a248616eb14bbc9efd85a662
Legendsxxxl <815605944@qq.com>
2023-12-18 20:24:58 +0800

    complete the test class

--- src/test/java/BrandServiceTest.java
+++ src/test/java/BrandServiceTest.java
@@ -1,14 +1,83 @@
+import com.sim.domain.Brand;
 import org.junit.Test;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public class BrandServiceTest {
     @Test
-    public void upload() {
+    public void getAll(int currentPage, int pageSize, String key, String value){
 
-    }
+    };
+    @Test
+    public void getDelete(){
+
+    };
+
+    @Test
+    public void getAllCount(String key, String value){
+
+    };
 
     @Test
     public void getDeleteCount(){
 
-    }
+    };
+
+    @Test
+    public void getById(int id){
+
+    };
+
+    @Test
+    public void save(Brand brand){
+
+    };
+
+    @Test
+    public void update(Brand brand){
+
+    };
+
+    @Test
+    public void delete(int id){
+
+    };
+
+    @Test
+    public void deleteByIds(int[] ids){
+
+    };
+
+    @Test
+    public void recover(int id){
+
+    };
+
+    @Test
+    public void deleteById(int id){
+
+    };
+
+    @Test
+    public void toggleSelect(int id){
+
+    };
+
+    @Test
+    public void upload(MultipartFile file){
+
+    };
+
+    @Test
+    public void getBySelect(){
+
+    };
+
+    @Test
+    public void getCountBySelect(){
+
+    };
+
+
 }
